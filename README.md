77ea3f530f7a5297d55966a32e9ffd00ed8e3287
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2019-01-03 13:02:54 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # nwjs-au-buefy
-vue template with autoupdate and buefy
+
+vue cli template with autoupdate and buefy
+
+
+```bash
+
+vue init hillar/nwjs-au-buefy <project_name>
+
+```
+
+## vue cli
+
+see https://cli.vuejs.org/guide/creating-a-project.html#pulling-2-x-templates-legacy
+
+## autoupdater
+
+see https://github.com/dsheiko/nw-autoupdater
+
+
+## buefy
+
+see https://github.com/buefy/buefy/
---
+++ meta.js
@@ -0,0 +1,49 @@
+module.exports = {
+  prompts: {
+    name: {
+      type: 'string',
+      required: true,
+      message: 'Application name'
+    },
+    label: {
+      type: 'string',
+      required: false,
+      message: 'Application conventional name',
+      default (data) {
+        return data.name.replace(/((?:^|-|_)+[a-z])/g, ($1) => $1.toUpperCase().replace('-', ''))
+      }
+    },
+    description: {
+      type: 'string',
+      required: false,
+      message: 'Application description',
+      default: 'A nwjs autoupdate buefy application'
+    },
+    plugins: {
+      type: 'checkbox',
+      message: 'Select which Vue plugins to install',
+      choices: ['axios'],
+      default: ['axios']
+    }
+  },
+  helpers: {
+    isEnabled (list, check, opts) {
+      if (list[check]) return opts.fn(this)
+      else return opts.inverse(this)
+    }
+  },
+  filters: {
+    'app/main/router/**/*': 'plugins[\'vue-router\']',
+    'app/main/store/**/*': 'plugins[\'vuex\']'
+  },
+  complete (data) {
+    console.log()
+    console.log('   Welcome to your new nwjs-au-buefy project!')
+    console.log()
+    console.log('   Next steps:')
+    if (!data.inPlace) console.log(`      \x1b[33m$\x1b[0m cd ${data.destDirName}`)
+    console.log('      \x1b[33m$\x1b[0m npm install')
+    console.log('      \x1b[33m$\x1b[0m npm run dev')
+    console.log('      \x1b[33m$\x1b[0m npm run build')
+  }
+}
