5b26c7cc38a52d0bfe000d45f382cff796083931
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2019-01-04 11:02:11 +0200

    fix bg dist dir

--- template/src/bg/bg.js
+++ template/src/bg/bg.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 // TODO move to 'central' config
 const __DEVPORT__ = {{ devport }}
 
-const winURL = isProduction ? 'dist/main/index.html' : `http://localhost:${__DEVPORT__}`
+const winURL = isProduction ? '{{ distdir }}/main/index.html' : `http://localhost:${__DEVPORT__}`
 const winOpts = nw.App.manifest.window
 
 winOpts.new_instance = true
